f63e236a307b645f0910ff6db64ec514024c7c43
faizdev007 <faizdev007@gmail.com>
2025-11-30 22:38:37 +0530

    add use client in

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import React, { useState, useEffect, useRef } from 'react';
 import { useRouter,usePathname } from "next/navigation";
 import Button from '../../components/ui/Button';
